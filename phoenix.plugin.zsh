382e2824df18d0b571982fb59b0e32dc1973440e
Kevin Sullivan <wolverineks@users.noreply.github.com>
2016-06-07 10:07:09 -0700

    Create phoenix.plugin.zsh, add some aliases

---
+++ phoenix.plugin.zsh
@@ -0,0 +1,35 @@
+# Phoenix Plugin Commands
+
+# GENERATORS
+
+# Generate Migration
+alias megm='mix ecto.gen.migration'
+
+# Generate HTML scaffolding
+alias mpgh='mix phoenix.gen.html'
+
+# Generate JSON scaffolding
+alias mpgj='mix phoenix.gen.json'
+
+# Generate Model
+alias mpgm='mix phoenix.gen.model'
+
+# DATABASE
+
+# Migrate the database
+alias mem='mix ecto.migrate'
+
+# Rollback the database
+alias mer='mix ecto.rollback'
+
+# CONSOLE/SERVER
+
+# Start Phoenix server
+alias mps='mix phoenix.server'
+
+# Start Phoenix Console
+alias pc='iex -S mix'
+
+# Start Elixir Console
+alias ec='iex'
+
