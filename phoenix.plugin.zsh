4a3bc5db30f0bbd5aa69d1dee5394098759eaad8
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-06-08 11:08:28 -0700

    More aliases

--- phoenix.plugin.zsh
+++ phoenix.plugin.zsh
@@ -14,6 +14,12 @@ alias mpgj='mix phoenix.gen.json'
 # Generate Model
 alias mpgm='mix phoenix.gen.model'
 
+# Generate Channel
+alias mpgc='mix phoenix.gen.channel'
+
+# Generate Secret
+alias mpgs='mix phoenix.gen.secret'
+
 # DATABASE
 
 # Migrate the database
@@ -21,6 +27,9 @@ alias mem='mix ecto.migrate'
 
 # Rollback the database
 alias mer='mix ecto.rollback'
+
+# Seed Database
+alias mrs='mix run priv/repo/seeds.exs'
 
 # CONSOLE/SERVER
 
@@ -33,3 +42,5 @@ alias pc='iex -S mix'
 # Start Elixir Console
 alias ec='iex'
 
+# Show Routes
+alias mpr='mix phoenix.routes'
